854201a702fb00ecebd98ef8c1c912ffdb8ccd41
ELNARMOTON79 <rafaelalexandro6949@gmail.com>
2025-09-25 17:08:22 -0600

    actualizaciones

---
+++ MaxMin/CustomButton.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+public class CustomButton : Button
+{
+    public CustomButton()
+    {
+        this.FlatStyle = FlatStyle.Flat;
+        this.FlatAppearance.BorderSize = 0;
+        this.BackColor = Color.Transparent;
+        this.ForeColor = Color.White;
+        this.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, ((byte)(0)));
+        this.TextAlign = ContentAlignment.MiddleLeft;
+        this.Padding = new Padding(20, 0, 0, 0);
+    }
+
+    protected override void OnPaint(PaintEventArgs pevent)
+    {
+        base.OnPaint(pevent);
+        Graphics g = pevent.Graphics;
+        g.SmoothingMode = SmoothingMode.AntiAlias;
+
+        // Fondo con gradiente y bordes redondeados
+        GraphicsPath path = new GraphicsPath();
+        int radius = 20;
+        Rectangle rect = new Rectangle(0, 0, this.Width, this.Height);
+        path.AddArc(rect.X, rect.Y, radius, radius, 180, 90);
+        path.AddArc(rect.X + rect.Width - radius, rect.Y, radius, radius, 270, 90);
+        path.AddArc(rect.X + rect.Width - radius, rect.Y + rect.Height - radius, radius, radius, 0, 90);
+        path.AddArc(rect.X, rect.Y + rect.Height - radius, radius, radius, 90, 90);
+        path.CloseAllFigures();
+
+        using (LinearGradientBrush brush = new LinearGradientBrush(rect,
+                                                                  Color.FromArgb(60, 80, 150),
+                                                                  Color.FromArgb(100, 50, 200),
+                                                                  LinearGradientMode.Horizontal))
+        {
+            g.FillPath(brush, path);
+        }
+
+        // Dibuja el nombre y la descripción separados
+        var titleFont = new Font("Segoe UI", 14F, FontStyle.Bold);
+        var descFont = new Font("Segoe UI", 10F, FontStyle.Regular);
+        var textBrush = new SolidBrush(this.ForeColor);
+
+        // Nombre (Text)
+        var titleRect = new Rectangle(this.Padding.Left, 18, this.Width - this.Padding.Horizontal, 28);
+        var sfTitle = new StringFormat { Alignment = StringAlignment.Near, LineAlignment = StringAlignment.Near };
+        g.DrawString(this.Text, titleFont, textBrush, titleRect, sfTitle);
+
+        // Descripción (Tag)
+        string descText = this.Tag?.ToString() ?? "";
+        var descRect = new Rectangle(this.Padding.Left, 50, this.Width - this.Padding.Horizontal - 10, this.Height - 60);
+        var sfDesc = new StringFormat { Alignment = StringAlignment.Near, LineAlignment = StringAlignment.Near };
+        g.DrawString(descText, descFont, textBrush, descRect, sfDesc);
+    }
+}
---
+++ MaxMin/grafica.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace MaxMin
+{
+    partial class grafica
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lbl_grafica = new Label();
+            btn_atras = new CustomButton();
+            SuspendLayout();
+            // 
+            // lbl_grafica
+            // 
+            lbl_grafica.AutoSize = true;
+            lbl_grafica.Font = new Font("Cooper Black", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lbl_grafica.ForeColor = Color.White;
+            lbl_grafica.Location = new Point(303, 27);
+            lbl_grafica.Name = "lbl_grafica";
+            lbl_grafica.Size = new Size(171, 46);
+            lbl_grafica.TabIndex = 1;
+            lbl_grafica.Text = "Grafica";
+            // 
+            // btn_atras
+            // 
+            btn_atras.BackColor = Color.FromArgb(50, 50, 100);
+            btn_atras.FlatStyle = FlatStyle.Flat;
+            btn_atras.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn_atras.ForeColor = Color.White;
+            btn_atras.Location = new Point(31, 22);
+            btn_atras.Name = "btn_atras";
+            btn_atras.Padding = new Padding(20, 0, 0, 0);
+            btn_atras.Size = new Size(99, 69);
+            btn_atras.TabIndex = 2;
+            btn_atras.Text = "Atrás";
+            btn_atras.TextAlign = ContentAlignment.MiddleLeft;
+            btn_atras.UseVisualStyleBackColor = false;
+            btn_atras.Click += btn_atras_Click;
+            // 
+            // grafica
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(30, 30, 60);
+            ClientSize = new Size(800, 450);
+            Controls.Add(lbl_grafica);
+            Controls.Add(btn_atras);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "grafica";
+            Text = "metodo_simplex";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private CustomButton btn_atras;
+        private Label lbl_grafica;
+    }
+}
