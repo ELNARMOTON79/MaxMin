13f8bfdf5cb9a3f7d6c32dfa485d98f49b25ab0c
ELNARMOTON79 <rafaelalexandro6949@gmail.com>
2025-09-25 19:54:04 -0600

    menu estilizado

--- MaxMin/Menu.Designer.cs
+++ MaxMin/Menu.Designer.cs
@@ -34,81 +34,80 @@
             btn_grafica = new Button();
             SuspendLayout();
             // 
+            // menu (form)
+            // 
+            this.AutoScaleDimensions = new SizeF(8F, 20F);
+            this.AutoScaleMode = AutoScaleMode.Font;
+            this.BackColor = Color.FromArgb(45, 45, 48);
+            this.ClientSize = new Size(1600, 1000);
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "menu";
+            this.Text = "MaxMin";
+            this.Load += menu_Load;
+            // 
             // lbl_max
             // 
             lbl_max.AutoSize = true;
             lbl_max.BackColor = Color.Transparent;
-            lbl_max.Font = new Font("Cooper Black", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            lbl_max.ForeColor = Color.Black;
-            lbl_max.Location = new Point(507, 177);
+            lbl_max.Font = new Font("Segoe UI", 32F, FontStyle.Bold);
+            lbl_max.ForeColor = Color.White;
+            lbl_max.Location = new Point(650, 80);
             lbl_max.Name = "lbl_max";
-            lbl_max.Size = new Size(188, 46);
+            lbl_max.Size = new Size(350, 70);
             lbl_max.TabIndex = 0;
             lbl_max.Text = "Max Min";
             // 
             // btn_simplex
             // 
-            btn_simplex.BackColor = SystemColors.Control;
+            btn_simplex.BackColor = Color.FromArgb(60, 80, 150);
             btn_simplex.FlatStyle = FlatStyle.Flat;
-            btn_simplex.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btn_simplex.ForeColor = Color.Black;
-            btn_simplex.Location = new Point(20, 30);
+            btn_simplex.Font = new Font("Segoe UI", 16F, FontStyle.Bold);
+            btn_simplex.ForeColor = Color.White;
+            btn_simplex.Location = new Point(600, 200);
             btn_simplex.Name = "btn_simplex";
-            btn_simplex.Padding = new Padding(20, 0, 0, 0);
-            btn_simplex.Size = new Size(381, 102);
-            btn_simplex.TabIndex = 0;
+            btn_simplex.Size = new Size(400, 80);
+            btn_simplex.TabIndex = 1;
             btn_simplex.Text = "Método Simplex";
-            btn_simplex.TextAlign = ContentAlignment.MiddleLeft;
             btn_simplex.UseVisualStyleBackColor = false;
             // 
             // btn_m
             // 
-            btn_m.BackColor = SystemColors.Control;
+            btn_m.BackColor = Color.FromArgb(60, 80, 150);
             btn_m.FlatStyle = FlatStyle.Flat;
-            btn_m.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btn_m.ForeColor = Color.Black;
-            btn_m.Location = new Point(20, 155);
+            btn_m.Font = new Font("Segoe UI", 16F, FontStyle.Bold);
+            btn_m.ForeColor = Color.White;
+            btn_m.Location = new Point(600, 320);
             btn_m.Name = "btn_m";
-            btn_m.Padding = new Padding(20, 0, 0, 0);
-            btn_m.Size = new Size(381, 116);
-            btn_m.TabIndex = 1;
+            btn_m.Size = new Size(400, 80);
+            btn_m.TabIndex = 2;
             btn_m.Text = "Método de la M";
-            btn_m.TextAlign = ContentAlignment.MiddleLeft;
             btn_m.UseVisualStyleBackColor = false;
             // 
             // btn_grafica
             // 
-            btn_grafica.BackColor = SystemColors.Control;
+            btn_grafica.BackColor = Color.FromArgb(60, 80, 150);
             btn_grafica.FlatStyle = FlatStyle.Flat;
-            btn_grafica.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btn_grafica.ForeColor = Color.Black;
-            btn_grafica.Location = new Point(20, 295);
+            btn_grafica.Font = new Font("Segoe UI", 16F, FontStyle.Bold);
+            btn_grafica.ForeColor = Color.White;
+            btn_grafica.Location = new Point(600, 440);
             btn_grafica.Name = "btn_grafica";
-            btn_grafica.Padding = new Padding(20, 0, 0, 0);
-            btn_grafica.Size = new Size(381, 100);
-            btn_grafica.TabIndex = 2;
+            btn_grafica.Size = new Size(400, 80);
+            btn_grafica.TabIndex = 3;
             btn_grafica.Text = "Gráfica de Programación Lineal";
-            btn_grafica.TextAlign = ContentAlignment.MiddleLeft;
             btn_grafica.UseVisualStyleBackColor = false;
             btn_grafica.Click += btn_grafica_Click;
             // 
-            // menu
+            // menu (add controls)
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
-            AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.White;
-            ClientSize = new Size(800, 450);
-            Controls.Add(lbl_max);
-            Controls.Add(btn_simplex);
-            Controls.Add(btn_m);
-            Controls.Add(btn_grafica);
-            MaximizeBox = false;
-            MinimizeBox = false;
-            Name = "menu";
-            Text = "MaxMin";
-            Load += menu_Load;
-            ResumeLayout(false);
-            PerformLayout();
+            this.Controls.Add(lbl_max);
+            this.Controls.Add(btn_simplex);
+            this.Controls.Add(btn_m);
+            this.Controls.Add(btn_grafica);
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
         #endregion
 
